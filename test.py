4e752d1a4c085f4e97a67a972e2d4b28f0bd0bd4
Louise Harms <tolouiseharms@gmail.com>
2024-09-24 15:18:11 +0200

    second

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+def input_list():
+    input_list = []
+    input_str = input("Enter numbers: ").split()
+    for num in input_str:
+        input_list.append(int(num))
+    return input_list
+
+def add_list(input_list):
+    return sum(input_list)
+
+print(add_list(input_list()))
